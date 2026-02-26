59a2b2c6847ba07164523a708ac41b9dc234b32d
AGonzales-9R <gonzales.am@obmontessori.edu.ph>
2026-02-27 07:55:00 +0800

    Update intrams.py

--- intrams.py
+++ intrams.py
@@ -75,16 +75,16 @@ def players(e):
     elif fill == 'yes' and sec == 'saph':
         display(f'''
         Here is your lineup of players:
-        Bautista
-        Macapagal
+        Aseo
+        Hizon
+        Yao
         Mendoza
-        Lumauig
-        Tolentino
-        Magbanua
-        Salvador
-        Dagohoy
-        Aquino
-        Laxamana
+        Ngo
+        Ko
+        Intalan
+        Uy
+        Rivera
+        Lagman
         ''', target='listOfPlayers')
 
     elif fill == 'yes' and sec == 'tpz':
@@ -103,5 +103,6 @@ def players(e):
 
     else:
         display(f'Please answer the form above.', target='listOfPlayers')
+
 
 
